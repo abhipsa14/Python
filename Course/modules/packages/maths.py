fefa863d0b698850405df80abec4968faa3db085
abhipsa14 <abhipsasri8183@gmail.com>
2025-07-16 15:09:23 +0530

    course-added

---
+++ Course/modules/packages/maths.py
@@ -0,0 +1,7 @@
+def addition(a,b):
+    return a+b
+def subtraction(a,b):
+    if a>b:
+        return a-b
+    else:
+        return b-a
---
+++ Course/modules/test.py
@@ -0,0 +1,5 @@
+from packages.maths import *
+from packages.subpackages.mul import multiply
+print(addition(9,8))
+print(subtraction(9,8))
+print(multiply(8,6))
